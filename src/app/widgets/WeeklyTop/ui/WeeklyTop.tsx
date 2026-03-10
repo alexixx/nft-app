b3beffc66beaed681794d3397005daf890e7a7b7
al3314 <al3314@yandex.ru>
2026-03-10 03:02:23 +0300

    fix: change mock data

--- src/app/widgets/WeeklyTop/ui/WeeklyTop.tsx
+++ src/app/widgets/WeeklyTop/ui/WeeklyTop.tsx
@@ -1,40 +1,72 @@
 'use client';
-import { useSelector } from 'react-redux';
+
 import styles from './WeeklyTop.module.scss';
 import { Slider } from '@/shared/ui';
 import { NftCard, NftCardSkeleton, useGetWeeklyNftsQuery } from '@/entities/Nft';
 
 const MOCK_NFTS = [
   {
-    id: 1,
+    id: 'anarkey-by-tradeai',
+    name: 'AnarKey',
+    endTime: '2026-03-14T16:56:15.149Z',
+    imageUrl: '/assets/nft/1.jpg',
+    bid: '8.75',
   },
   {
-    id: 1,
+    id: 'the-anata-nft',
+    name: 'The Anata NFT',
+    endTime: '2026-03-14T13:39:07.968Z',
+    imageUrl: '/assets/nft/5.jpg',
+    bid: '0.53',
   },
   {
-    id: 1,
+    id: 'today-ancient-seed',
+    name: 'TODAY - Ancient Seed',
+    endTime: '2026-03-14T11:24:12.061Z',
+    imageUrl: '/assets/nft/4.jpg',
+    bid: '1.13',
   },
   {
-    id: 1,
+    id: 'arcadenft',
+    name: 'ArcadeNFT',
+    endTime: '2026-03-14T05:47:30.785Z',
+    imageUrl: '/assets/nft/5.jpg',
+    bid: '0.40',
   },
   {
-    id: 1,
+    id: 'cute-animals',
+    name: 'Cute Animals',
+    endTime: '2026-03-14T11:58:22.718Z',
+    imageUrl: '/assets/nft/3.jpg',
+    bid: '3.23',
   },
   {
-    id: 1,
-  },
-  {
-    id: 1,
-  },
-  {
-    id: 1,
+    id: 'a-n-i-m-o',
+    name: 'A.N.I.M.O',
+    endTime: '2026-03-14T22:23:50.361Z',
+    imageUrl: '/assets/nft/4.jpg',
+    bid: '6.39',
   },
 ];
 
 export const WeeklyTop = () => {
   const { data: nfts, isLoading, isError } = useGetWeeklyNftsQuery();
 
-  if (isError) return <div>Ошибка при загрузке данных</div>;
+  if (isError)
+    return (
+      <section className={styles.section}>
+        <h1 className={styles.title}>Weekly - Top NFT</h1>
+
+        <div className={styles.sliderWrapper}>
+          <Slider>
+            {MOCK_NFTS.map((nft, i) => (
+              // <NftCardSkeleton key={i} />
+              <NftCard key={i} {...nft} />
+            ))}
+          </Slider>
+        </div>
+      </section>
+    );
 
   return (
     <section className={styles.section}>
